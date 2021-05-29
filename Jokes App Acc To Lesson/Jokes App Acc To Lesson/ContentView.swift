ee90621794b771a1feb77130c3c07135aaa803dc
Ang Jun Ray <angjunray@live.com>
2021-05-29 11:41:42 +0800

    default commits

---
+++ Jokes App Acc To Lesson/Jokes App Acc To Lesson/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Jokes App Acc To Lesson
+//
+//  Created by Ang Jun Ray on 29/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Jokes App Acc To Lesson/Jokes App Acc To Lesson/Jokes_App_Acc_To_LessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jokes_App_Acc_To_LessonApp.swift
+//  Jokes App Acc To Lesson
+//
+//  Created by Ang Jun Ray on 29/5/21.
+//
+
+import SwiftUI
+
+@main
+struct Jokes_App_Acc_To_LessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
